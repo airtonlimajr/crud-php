d67382f5977cf4118926ea8d40564f75482c3f92
Airton Lima Jr <airtonlimajr@outlook.com>
2022-03-27 00:29:54 -0300

    Adicionando a tela de login.

--- index.php
+++ index.php
@@ -1,62 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Menu</title>
+    <title>Tela de Login</title>
 </head>
+
+
+<style type="text/css">
+
+.container {
+    margin-top: 10%;
+    width: 500px;
+    font-family: 'Roboto', sans-serif;
+    -webkit-box-shadow: 22px 17px 36px -31px rgba(0,0,0,0.75);
+-moz-box-shadow: 22px 17px 36px -31px rgba(0,0,0,0.75);
+box-shadow: 22px 17px 36px -31px rgba(0,0,0,0.75);
+}
+</style>
 <body>
 
-<div class="container" style="margin-top:100px">
-<div class="row">
 
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar Produtos</h5>
-        <p class="card-text">Opção para adicionar produtos em nosso estoque.</p>
-        <a href="adicionar_produto.php" class="btn btn-primary">Cadastrar Produtos</a>
-      </div>
+    <div class="container" style="border-radius:15px; border: 2px solid #F3F3F3" >
+        <div style="padding: 10px">
+            <div class="img-locked" style="text-align: center">
+                <img src="https://cdn-icons-png.flaticon.com/512/3427/3427189.png" width="125px" height="125px" >
+            </div>
+            <form>
+                <div class="form-group">
+                    <label>Usuário</label>
+                    <input type="text" name="usuario" class="form-control" placeholder="Usuário" autocomplete="off" required>
+                </div>
+                <div class="form-group">
+                    <label>Senha</label>
+                    <input type="password" name="usuario" class="form-control" placeholder="Senha" autocomplete="off" required>
+                </div>
+            </form>
+            <div style="text-align: right" >
+            <button type="submit"  class="btn btn-sm btn-success" style="margin-top: 5%">Entrar</button>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="col-sm-6" >
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Lista de Produtos</h5>
-        <p class="card-text">Visualizar, editar e excluir os produtos.</p>
-        <a href="listar_produtos.php" class="btn btn-primary">Produtos</a>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-sm-6"style="margin-top:20px">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar Categoria</h5>
-        <p class="card-text">Opção para adicionar categoria de produtos.</p>
-        <a href="adicionar_categoria.php" class="btn btn-primary">Cadastrar Categoria</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6" style="margin-top:20px">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar fornecedor</h5>
-        <p class="card-text">Opção para adicionar fornecedores.</p>
-        <a href="adicionar_fornecedor.php" class="btn btn-primary">Cadastrar Fornecedor</a>
-      </div>
-    </div>
-  </div>
-
-</div>
-
-</div>
 
 
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></scrip>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" ></script>
+</body> 
 </html>
---
+++ menu.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Menu</title>
+</head>
+<body>
+
+<div class="container" style="margin-top:100px">
+<div class="row">
+
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Adicionar Produtos</h5>
+        <p class="card-text">Opção para adicionar produtos em nosso estoque.</p>
+        <a href="adicionar_produto.php" class="btn btn-primary">Cadastrar Produtos</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6" >
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Lista de Produtos</h5>
+        <p class="card-text">Visualizar, editar e excluir os produtos.</p>
+        <a href="listar_produtos.php" class="btn btn-primary">Produtos</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6"style="margin-top:20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Adicionar Categoria</h5>
+        <p class="card-text">Opção para adicionar categoria de produtos.</p>
+        <a href="adicionar_categoria.php" class="btn btn-primary">Cadastrar Categoria</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6" style="margin-top:20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Adicionar fornecedor</h5>
+        <p class="card-text">Opção para adicionar fornecedores.</p>
+        <a href="adicionar_fornecedor.php" class="btn btn-primary">Cadastrar Fornecedor</a>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
